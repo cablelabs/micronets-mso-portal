c940b99ee3ae9b405a6d6321e5727d036ed393e0
Ashwini Kadam <a.kadam@cablelabs.com>
2018-02-26 10:18:04 -0800

    Added authentication for JWT

--- src/services/subscribers/subscribers.hooks.js
+++ src/services/subscribers/subscribers.hooks.js
@@ -1,10 +1,8 @@
-//const { authenticate } = require('feathers-authentication').hooks;
+const { authenticate } = require('feathers-authentication').hooks;
 
 module.exports = {
   before: {
-    all: [
-      //authenticate('jwt')
-    ],
+    all: [ authenticate('jwt') ],
     find: [],
     get: [
       hook => {
