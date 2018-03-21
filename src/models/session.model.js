b58fb0ed921288eb4cc8aa7c045aca36351799d3
Ashwini Kadam <a.kadam@cablelabs.com>
2018-03-20 17:18:39 -0700

    Session model updates

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -2,15 +2,17 @@
 // 
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
+
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const session = new Schema({
-    token: { type: String, required: false },
-    subscriberId: { type: String, required: true },
-    deviceId: { type: String, required: true },
-    clientId: { type: String, required: true },
-    macAddress: { type: String, required: true }
+        id: { type: String, required: true,  unique: true, primaryKey: true },
+        devices:[{
+            deviceId: { type: String, required: true },
+            clientId: { type: String, required: true },
+            macAddress: { type: String, required: true }
+          }]
   }, {
     timestamps: true
   });
