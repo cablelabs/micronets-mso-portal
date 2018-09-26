89a654893edba10a796f0b0a017ee135d4790f49
Ashwini Kadam <a.kadam@cablelabs.com>
2018-09-26 12:04:44 -0600

    Merge branch 'issue-mm-apis' into issue-mm-api-refactor
    # Conflicts:
    #	src/services/ca/ca.hooks.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
-const io = require('socket.io').listen(server);
+const io = require('socket.io')(server);
+
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -14,15 +15,35 @@ server.on('listening', () =>
   logger.info('Feathers application started on %s:%d ', app.get('host'), port)
 );
 
-io.on('connection' , (() => logger.info('Socket io connection ')))
 
-io.on('connection' , (socket) => {
-  app.service('/portal/session').on('sessionCreate' ,(data) => {
-    // console.log('\n FeatherJS event sessionCreate fired with data : ' + JSON.stringify(data))
+app.service('/portal/session').on('sessionCreate' ,(data) => {
+  console.log('\n FeatherJS event sessionCreate fired with data : ' + JSON.stringify(data));
+  io.on('connection' , (socket) => {
+    console.log('Socket IO connection ' + JSON.stringify(socket.id));
     socket.emit('socketSessionCreate', data);
-  });
-  app.service('/portal/session').on('sessionUpdate' ,(data) => {
-    // console.log('\n FeatherJS event sessionUpdate fired with data : ' + JSON.stringify(data))
-    socket.emit('socketSessionUpdate', data);
+    socket.on('disconnect', () => {
+      console.log('\n Socket IO disconnect' + JSON.stringify(socket.id))
+      socket.removeAllListeners('send message');
+      socket.removeAllListeners('disconnect');
+      socket.removeAllListeners('connection');
+      socket.disconnect( true );
+    });
   });
 });
+
+app.service('/portal/session').on('sessionUpdate' ,(data) => {
+  console.log ( '\n FeatherJS event sessionUpdate fired with data : ' + JSON.stringify ( data ) );
+  io.on ( 'connection' , ( socket ) => {
+    console.log ( 'Socket IO connection ' + JSON.stringify ( socket.id ) );
+    socket.emit ( 'socketSessionUpdate' , data );
+    socket.on('disconnect', () => {
+      console.log('\n Socket IO disconnect' + JSON.stringify(socket.id))
+      socket.removeAllListeners('send message');
+      socket.removeAllListeners('disconnect');
+      socket.removeAllListeners('connection');
+      socket.disconnect( true );
+    });
+  });
+})
+
+
--- api/services/session/session.hooks.js
+++ api/services/session/session.hooks.js
@@ -5,10 +5,21 @@ module.exports = {
   before : {
     all : [ authenticate ( 'jwt' ) ] ,
     find : [] ,
-    get : [] ,
+    get : [
+      hook => {
+        const {params, id} = hook
+        const query = Object.assign({ id: id ? id : params.id }, hook.params.query);
+        console.log('\n Get hook session query : ' + JSON.stringify(query))
+        return hook.app.service('/portal/session').find({ query })
+          .then( ({ data }) => {
+            hook.result = omitMeta(data[0]);
+            console.log('\n hook.result  : ' + JSON.stringify(hook.result));
+          });
+      }
+    ] ,
     create : [
       ( hook ) => {
-        const { data , params } = hook;
+        const { data , params, app } = hook;
         hook.data = Object.assign ( {} , {
           id : data.subscriberId ,
           name : data.name ,
@@ -20,30 +31,48 @@ module.exports = {
             class: params.payload.class
           } ) ]
         } );
-        hook.app.service ( '/portal/session' ).emit ( 'sessionCreate' , {
+        app.service ( '/portal/session' ).emit ( 'sessionCreate' , {
           type : 'sessionCreate' ,
-          data : { subscriberId : hook.data.id  }
+          data : { subscriberId : hook.data.id , device: Object.assign({}, { deviceId:params.payload.deviceID , class:params.payload.class, macAddress:params.payload.macAddress }) }
         } );
       }
     ] ,
-    update : [
+    update : [] ,
+    patch : [
       hook => {
-        const { id } = hook;
-        return hook.app.service ( '/portal/session' ).find ( { query : { id : id } } )
+        const { id, app } = hook;
+        hook.params.mongoose = {
+          runValidators: true,
+          setDefaultsOnInsert: true
+        }
+        console.log('\n Session patch method hook.data : ' + JSON.stringify(hook.data))
+        const eventData = Object.assign({},{deviceId:hook.data.deviceId , class:hook.data.class, macAddress:hook.data.macAddress})
+        console.log('\n EventData : ' + JSON.stringify(eventData))
+        const query = Object.assign({ id: id ? id : hook.params.id }, hook.params.query);
+        return hook.app.service ( '/portal/session' ).find (  {query}  )
           .then ( ( { data } ) => {
             const originalSession = data[ 0 ];
             console.log('\n Original Session : ' + JSON.stringify(originalSession))
-            let updatedSession = Object.assign ( {} , originalSession , originalSession.devices.push ( hook.data ) );
-            console.log('\n Updated Session : ' + JSON.stringify(updatedSession))
-            hook.data = Object.assign ( {} , updatedSession );
-            hook.app.service ( '/portal/session' ).emit ( 'sessionUpdate' , {
-              type : 'sessionUpdate' ,
-              data : { subscriberId : hook.data.id  }
-            } );
+            const foundDeviceIndex = originalSession.devices.findIndex( device => device.clientId ==  hook.data.clientId && device.deviceId == hook.data.deviceId && device.macAddress == hook.data.macAddress && device.class == hook.data.class);
+            console.log('\n Hook session foundDeviceIndex : ' + JSON.stringify(foundDeviceIndex))
+
+            if(foundDeviceIndex >= 0 ) {
+              console.log('\n Nothing to do.Device already present')
+              return Promise.resolve(hook);
+            }
+
+            if(foundDeviceIndex == -1 ) {
+              let updatedSession = Object.assign ( {} , originalSession , originalSession.devices.push ( hook.data ) );
+              console.log ( '\n Updated Session : ' + JSON.stringify ( updatedSession ) )
+              hook.data = Object.assign ( {} , updatedSession );
+              app.service ( '/portal/session' ).emit ( 'sessionUpdate' , {
+                type : 'sessionUpdate' ,
+                data : { subscriberId : hook.data.id , device : eventData }
+              } );
+            }
           } );
       }
     ] ,
-    patch : [] ,
     remove : []
   } ,
 
@@ -53,13 +82,24 @@ module.exports = {
     get : [] ,
     create : [
       hook => {
-        const { params , data , payload } = hook;
+        const { params , data , payload, app } = hook;
+        console.log('\n Update create after hook Params : ' + JSON.stringify(params) + '\t\t Data : ' + JSON.stringify(data) + '\t\t\t Payload : ' + JSON.stringify(payload))
+        console.log('\n Update create after hook hook.data : ' + JSON.stringify(hook.data))
         hook.result = omitMeta ( hook.data );
-        console.log('\n Session hook result : ' + JSON.stringify(hook.result))
+        console.log('\n Session hook result : ' + JSON.stringify(hook.result));
       }
     ] ,
-    update : [] ,
-    patch : [] ,
+    update : [
+
+    ] ,
+    patch : [
+      hook => {
+        const {id, params, payload} = hook
+        console.log('\n Session patch after hook PARAMS : ' + JSON.stringify(params) + '\t\t ID : ' + JSON.stringify(id) + '\t\t\t PAYLOAD : ' + JSON.stringify(payload))
+        hook.result = omitMeta ( hook.data );
+        console.log('\n Session patch after hook result : ' + JSON.stringify(hook.result));
+      }
+    ] ,
     remove : []
   } ,
 
