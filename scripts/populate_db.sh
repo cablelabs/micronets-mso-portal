4c073515ff09e8d8befa917542bea132713fb719
Ashwini Kadam <a.kadam@cablelabs.com>
2018-12-16 20:37:52 -0800

    Added scripts to populate db

---
+++ scripts/populate_db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+MSO_PORTAL_HOSTNAME='nccoe-mso-api.micronets.in'
+MM_HOSTNAME='nccoe-mm-api.micronets.in'
+
+echo 'Getting Authorization token from ' $MSO_PORTAL_HOSTNAME
+
+TOKEN=$(curl -s -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' --data-binary @./data/tokenPost.json http://${MSO_PORTAL_HOSTNAME}/portal/registration/token | jq -r '.accessToken')
+JWT_TOKEN="Bearer ${TOKEN}"
+
+echo ' JWT Token : ' $JWT_TOKEN
+
+#! POST SUBSCRIBER CURL
+
+echo ' Populating subscriber on ' $MSO_PORTAL_HOSTNAME
+
+SUBSCRIBER=$(curl -s -X POST -H "Authorization: ${JWT_TOKEN}" -H 'Accept: application/json' -H 'Content-Type: application/json' --data-binary @./data/subscriberPost.json http://${MSO_PORTAL_HOSTNAME}/internal/subscriber )
+
+echo ' Subscriber : ' ${SUBSCRIBER}
