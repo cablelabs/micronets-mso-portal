8425cafe9e412a0893f8bb64482b529d91ead451
Ashwini Kadam <a.kadam@cablelabs.com>
2018-12-20 10:31:30 -0800

    config updates for linode instance

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -4,5 +4,5 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_URL: '"http://127.0.0.1:8081"',
-  MSO_PORTAL_BASE_URL:'"http://127.0.0.1:3210"'
+  MSO_PORTAL_BASE_URL:'"https://mso-portal-api.micronets.in"'
 })
