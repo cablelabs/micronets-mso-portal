6a4daea2f020f66d03c74b6a17db3d9e0da2bc2f
Ashwini Kadam <a.kadam@cablelabs.com>
2019-05-28 14:46:52 -0600

    Linode config updates

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://10.70.17.163:8081"',
-  MSO_PORTAL_BASE_URL:'"http://10.70.17.163:3210"'
+  BASE_URL: '"http://45.33.1.184:8081"',
+  MSO_PORTAL_BASE_URL:'"http://45.33.1.184:3210"'
 })
