8a09bc89a58314a5942258ef4f237c46de7a2ec6
Ashwini Kadam <a.kadam@cablelabs.com>
2019-05-28 12:45:08 -0600

    Config updates

--- app/config/dev.env.js
+++ app/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://127.0.0.1:8081"',
-  MSO_PORTAL_BASE_URL:'"http://127.0.0.1:3210"'
+  BASE_URL: '"http://10.70.17.163:8081"',
+  MSO_PORTAL_BASE_URL:'"http://10.70.17.163:3210"'
 })
