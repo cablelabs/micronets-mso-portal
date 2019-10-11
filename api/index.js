018c4422c2f00a7b717ba22d667ca4375dfc499f
Ashwini Kadam <a.kadam@cablelabs.com>
2019-10-11 13:22:09 -0700

    Logging

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = require('./app');
 const port = app.get('listenPort');
 const host = app.get('listenHost');
 const webSocketBaseUrl = app.get('webSocketBaseUrl')
+const publicApiBaseUrl = app.get('publicApiBaseUrl')
 const server = app.listen(port,host);
 
 process.on('unhandledRejection', (reason, p) =>
@@ -14,6 +15,7 @@ server.on('listening', () => {
   address = server.address()
   logger.info ('Feathers application started on ' + JSON.stringify(`http://${address.address}:${address.port}`))
   logger.info ('Feathers  webSocketBaseUrl ' + JSON.stringify(webSocketBaseUrl))
+  logger.info ('Feathers  publicApiBaseUrl ' + JSON.stringify(publicApiBaseUrl))
 });
 
 
